06587c5bc820c0d99629cd5104b93999a30f10b0
andrew.elsen@yahoo.com <andrew.elsen@yahoo.com@ff96d080-0a19-11df-8158-c7cbc0dd6240>
2010-02-09 00:53:22 +0000

    Initial Code

--- BreakawayRobot/com/team1987/breakaway/BreakawayRobot.java
+++ BreakawayRobot/com/team1987/breakaway/BreakawayRobot.java
@@ -4,11 +4,13 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package com.team1987.breakaway;
 
-
+import edu.wpi.first.wpilibj.Watchdog;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Joystick;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,26 +20,43 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class BreakawayRobot extends IterativeRobot {
+
+    RobotDrive m_robotDrive;		// robot will use PWM 1-4 for drive motors
+    Joystick m_rightStick;			// joystick 1 (arcade stick or right tank stick)
+    Joystick m_leftStick;			// joystick 2 (tank left stick)
+
+    public BreakawayRobot() {
+        // Create a robot using standard right/left robot drive on PWMS 1, 2, 3, and 4
+        m_robotDrive = new RobotDrive(1, 2, 3, 4);
+
+        // Define joysticks being used at USB port #1 and USB port #2 on the Drivers Station
+        m_rightStick = new Joystick(1);
+        m_leftStick = new Joystick(2);
+    }
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+		// feed the user watchdog at every period when in autonomous
+		Watchdog.getInstance().feed();
+
+        m_robotDrive.arcadeDrive(m_rightStick, false);			// drive with arcade style (use right stick)
+
+
+
     }
-    
 }
