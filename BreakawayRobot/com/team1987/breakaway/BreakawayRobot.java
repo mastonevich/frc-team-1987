0d4c2e43815f6f6eb5766625f31c58c4c532586b
andrew.elsen@yahoo.com <andrew.elsen@yahoo.com@ff96d080-0a19-11df-8158-c7cbc0dd6240>
2010-02-17 02:27:00 +0000

    Added herder & kicker solenoids (and wired to button 1), added kicker winder victor, added combine relay
    clarified names

--- BreakawayRobot/com/team1987/breakaway/BreakawayRobot.java
+++ BreakawayRobot/com/team1987/breakaway/BreakawayRobot.java
@@ -26,7 +26,6 @@ import edu.wpi.first.wpilibj.DriverStationLCD;
 
 import com.team1987.breakaway.api.Constants;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -39,32 +38,36 @@ public class BreakawayRobot extends IterativeRobot {
     DriverStation m_DS;
     DriverStationEnhancedIO m_DSEIO;
     DriverStationLCD m_DSLCD;
-
     RobotDrive m_robotDrive;		// robot will use PWM 1-4 for drive motors
     Joystick m_rightStick;			// joystick 1 (arcade stick or right tank stick)
     public Joystick m_leftStick;			// joystick 2 (tank left stick)
     Compressor m_compressor;
-    Solenoid m_solenoid1;
-    Solenoid m_solenoid2;
-    Relay m_relay1;
+    //Solenoids
+    Solenoid m_LanceAngleSolenoidIn;
+    Solenoid m_LanceAngleSolenoidOut;
+    Solenoid m_herderSolenoid1In;
+    Solenoid m_herderSolenoid1Out;
+    Solenoid m_herderSolenoid2In;
+    Solenoid m_herderSolenoid2Out;
+    Solenoid m_kickerSolenoidIn;
+    Solenoid m_kickerSolenoidOut;
+    Relay m_LanceExtenderRelay;
+    Relay m_combineRelay;
     AnalogChannel m_analogChannel1;
-    Victor m_victor1;
-   
-
+    Victor m_kickerWinder;
+    Victor m_winch;
     double kScoreThreshold = .01;
     AxisCamera cam;
-
     TrackerDashboard trackerDashboard = new TrackerDashboard();
-
     int kickerStrength;
 
     public BreakawayRobot() {
 
-       m_DS = DriverStation.getInstance();
+        m_DS = DriverStation.getInstance();
 
-       m_DSEIO = m_DS.getEnhancedIO();
+        m_DSEIO = m_DS.getEnhancedIO();
 
-       m_DSLCD = DriverStationLCD.getInstance();
+        m_DSLCD = DriverStationLCD.getInstance();
 
 
         // Create a robot using standard right/left robot drive on PWMS 1, 2, 3, and 4
@@ -76,22 +79,40 @@ public class BreakawayRobot extends IterativeRobot {
         m_leftStick = new Joystick(Constants.c_joystickLeftPort);
         m_compressor = new Compressor(Constants.c_compressorPressureSwitchChannel,
                 Constants.c_compressorRelayChannel);
-        m_solenoid1 = new Solenoid(Constants.c_hookAngleSolenoid1Channel);
-        m_solenoid2 = new Solenoid(Constants.c_hookAngleSolenoid2Channel);
-        m_relay1 = new Relay(Constants.c_hookExtenderChannel, Relay.Direction.kBoth);
+        m_LanceAngleSolenoidIn = new Solenoid(Constants.c_LanceAngleSolenoidInChannel);
+        m_LanceAngleSolenoidOut = new Solenoid(Constants.c_LanceAngleSolenoidOutChannel);
+        m_herderSolenoid1In = new Solenoid(Constants.c_herderSolenoid1InChannel);
+        m_herderSolenoid1Out = new Solenoid(Constants.c_herderSolenoid1OutChannel);
+        m_herderSolenoid2In = new Solenoid(Constants.c_herderSolenoid2InChannel);
+        m_herderSolenoid2Out = new Solenoid(Constants.c_herderSolenoid1OutChannel);
+        m_kickerSolenoidIn = new Solenoid(Constants.c_kickerSolenoidInChannel);
+        m_kickerSolenoidOut = new Solenoid(Constants.c_kickerSolenoidOutChannel);
+
         m_analogChannel1 = new AnalogChannel(Constants.c_stringPOTChannel);
-        m_victor1 = new Victor(Constants.c_victor1Channel);
+        m_kickerWinder = new Victor(Constants.c_kickerWinderChannel);
+
+        m_LanceExtenderRelay = new Relay(Constants.c_LanceExtenderRelayChannel, Relay.Direction.kBoth);
+        m_combineRelay = new Relay(Constants.c_combineRelayChannel, Relay.Direction.kBoth);
 
     }
 
     public void robotInit() {
         m_compressor.start();
-        m_solenoid1.set(true);
-        m_solenoid2.set(false);
-        m_relay1.set(Relay.Value.kOff);
-        m_victor1.set(Constants.c_victorStop);
+        m_LanceAngleSolenoidIn.set(true);
+        m_LanceAngleSolenoidOut.set(false);
+        m_herderSolenoid1In.set(true);
+        m_herderSolenoid1Out.set(false);
+        m_herderSolenoid2In.set(true);
+        m_herderSolenoid2Out.set(false);
+        m_kickerSolenoidIn.set(true);
+        m_kickerSolenoidOut.set(false);
 
-        for(int i=1; i<9; i++) {
+        m_kickerWinder.set(Constants.c_victorStop);
+
+        m_LanceExtenderRelay.set(Relay.Value.kOff);
+        m_combineRelay.set(Relay.Value.kOff);
+
+        for(int i = 1; i < 9; i++) {
             try {
                 m_DSEIO.setDigitalConfig(i, DriverStationEnhancedIO.tDigitalConfig.kInputPullUp);
             } catch(DriverStationEnhancedIO.EnhancedIOException ex) {
@@ -121,9 +142,10 @@ public class BreakawayRobot extends IterativeRobot {
 
     public void teleopInit() {
         boolean lastTrigger = false;
-    DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1, "Starting Camera Code");
-    DriverStationLCD.getInstance().updateLCD();
-    Timer.delay(1.0); //Wait one second so user can see starting message
+        DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1,
+                "Starting Camera Code");
+        DriverStationLCD.getInstance().updateLCD();
+        Timer.delay(1.0); //Wait one second so user can see starting message
     }
 
     //Periodics
@@ -146,35 +168,57 @@ public class BreakawayRobot extends IterativeRobot {
         Watchdog.getInstance().feed();
 
         m_robotDrive.arcadeDrive(m_rightStick, false);			// drive with arcade style (use right stick)
-        if(m_rightStick.getRawButton(Constants.c_hookAngleButton)) {
-            m_solenoid1.set(false);
-            m_solenoid2.set(true);
+
+
+        //Herder Code
+        if(m_rightStick.getRawButton(Constants.c_kickerRightButton)) {
+            m_herderSolenoid1In.set(false);
+            m_herderSolenoid1Out.set(true);
+            m_herderSolenoid2In.set(false);
+            m_herderSolenoid2Out.set(true);
+            m_kickerSolenoidIn.set(false);
+            m_kickerSolenoidOut.set(true);
         }
         else {
-            m_solenoid1.set(true);
-            m_solenoid2.set(false);
+            m_herderSolenoid1In.set(true);
+            m_herderSolenoid1Out.set(false);
+            m_herderSolenoid2In.set(true);
+            m_herderSolenoid2Out.set(false);
+            m_kickerSolenoidIn.set(true);
+            m_kickerSolenoidOut.set(false);
         }
 
-        if(m_rightStick.getRawButton(Constants.c_hookExtenderButton)) {
+        //Lance Angle Code
+        if(m_leftStick.getRawButton(Constants.c_LanceAngleLeftButton)) {
+            m_LanceAngleSolenoidIn.set(false);
+            m_LanceAngleSolenoidOut.set(true);
+        }
+        else {
+            m_LanceAngleSolenoidIn.set(true);
+            m_LanceAngleSolenoidOut.set(false);
+        }
+
+        //Lance Extender Code
+        if(m_leftStick.getRawButton(Constants.c_LanceExtenderLeftButton)) {
             if(m_analogChannel1.getVoltage() > Constants.c_stringPOT_min) {
-                m_relay1.set(Relay.Value.kForward);
+                m_LanceExtenderRelay.set(Relay.Value.kForward);
             }
             else {
-                m_relay1.set(Relay.Value.kOff);
+                m_LanceExtenderRelay.set(Relay.Value.kOff);
             }
         }
-        else if(m_rightStick.getRawButton(Constants.c_hookRetractorButton)) {
+        else if(m_leftStick.getRawButton(Constants.c_LanceRetractorLeftButton)) {
             if(m_analogChannel1.getVoltage() < Constants.c_stringPOT_max) {
-                m_relay1.set(Relay.Value.kReverse);
+                m_LanceExtenderRelay.set(Relay.Value.kReverse);
             }
             else {
-                m_relay1.set(Relay.Value.kOff);
+                m_LanceExtenderRelay.set(Relay.Value.kOff);
             }
         }
         else {
-            m_relay1.set(Relay.Value.kOff);
+            m_LanceExtenderRelay.set(Relay.Value.kOff);
         }
-        
+
         if(m_leftStick.getZ() <= 0) {
             m_DSLCD.println(DriverStationLCD.Line.kUser2, 1, "Kicker Strength = " + kickerStrength());
             m_DSLCD.updateLCD();
@@ -182,38 +226,43 @@ public class BreakawayRobot extends IterativeRobot {
 
 
         try {
-                DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1, "Camera Running     ");
-                DriverStationLCD.getInstance().updateLCD();
-                if (cam.freshImage()) {// && turnController.onTarget()) {
-                    ColorImage image = cam.getImage();
-                    Thread.yield();
-                    Target[] targets = Target.findCircularTargets(image);
-                    Thread.yield();
-                    image.free();
-                    if (targets.length == 0 || targets[0].m_score < kScoreThreshold) {
-                        System.out.println("No target found");
-                        DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1, "Not Found              " );
-                        Target[] newTargets = new Target[targets.length + 1];
-                        newTargets[0] = new Target();
-                        newTargets[0].m_majorRadius = 0;
-                        newTargets[0].m_minorRadius = 0;
-                        newTargets[0].m_score = 0;
-                        for (int i = 0; i < targets.length; i++) {
-                            newTargets[i + 1] = targets[i];
-                        }
-                  } else {
-                        DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser3, 1, "Pos X: " + targets[0].m_xPos );
-                        DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser4, 1, "Pos Y: " + targets[0].m_yPos );
-                        DriverStationLCD.getInstance().updateLCD();
-                        System.out.println(targets[0]);
-                        System.out.println("Target Angle: " + targets[0].getHorizontalAngle());
+            DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1,
+                    "Camera Running     ");
+            DriverStationLCD.getInstance().updateLCD();
+            if(cam.freshImage()) {// && turnController.onTarget()) {
+                ColorImage image = cam.getImage();
+                Thread.yield();
+                Target[] targets = Target.findCircularTargets(image);
+                Thread.yield();
+                image.free();
+                if(targets.length == 0 || targets[0].m_score < kScoreThreshold) {
+                    System.out.println("No target found");
+                    DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1,
+                            "Not Found              ");
+                    Target[] newTargets = new Target[targets.length + 1];
+                    newTargets[0] = new Target();
+                    newTargets[0].m_majorRadius = 0;
+                    newTargets[0].m_minorRadius = 0;
+                    newTargets[0].m_score = 0;
+                    for(int i = 0; i < targets.length; i++) {
+                        newTargets[i + 1] = targets[i];
                     }
                 }
-            } catch (NIVisionException ex) {
-                ex.printStackTrace();
-            } catch (AxisCameraException ex) {
-                ex.printStackTrace();
+                else {
+                    DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser3, 1,
+                            "Pos X: " + targets[0].m_xPos);
+                    DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser4, 1,
+                            "Pos Y: " + targets[0].m_yPos);
+                    DriverStationLCD.getInstance().updateLCD();
+                    System.out.println(targets[0]);
+                    System.out.println("Target Angle: " + targets[0].getHorizontalAngle());
+                }
             }
+        } catch(NIVisionException ex) {
+            ex.printStackTrace();
+        } catch(AxisCameraException ex) {
+            ex.printStackTrace();
+        }
 
     }
 
@@ -229,23 +278,37 @@ public class BreakawayRobot extends IterativeRobot {
 
     public int kickerStrength() {
 
-            try {
-                kickerStrength = ~m_DSEIO.getDigitals() & Constants.c_kickerSwitchBits;
-                switch(kickerStrength) {
-                    case Constants.c_kickerSwitchPos1: break;
-                    case Constants.c_kickerSwitchPos2: break;
-                    case Constants.c_kickerSwitchPos3: kickerStrength = 3; break;
-                    case Constants.c_kickerSwitchPos4: kickerStrength = 4; break;
-                    case Constants.c_kickerSwitchPos5: kickerStrength = 5; break;
-                    case Constants.c_kickerSwitchPos6: kickerStrength = 6; break;
-                    case Constants.c_kickerSwitchPos7: kickerStrength = 7; break;
-                    case Constants.c_kickerSwitchPos8: kickerStrength = 8; break;
-                }
-
-            } catch(DriverStationEnhancedIO.EnhancedIOException ex) {
-                ex.printStackTrace();
+        try {
+            kickerStrength = ~m_DSEIO.getDigitals() & Constants.c_kickerSwitchBits;
+            switch(kickerStrength) {
+                case Constants.c_kickerSwitchPos1:
+                    break;
+                case Constants.c_kickerSwitchPos2:
+                    break;
+                case Constants.c_kickerSwitchPos3:
+                    kickerStrength = 3;
+                    break;
+                case Constants.c_kickerSwitchPos4:
+                    kickerStrength = 4;
+                    break;
+                case Constants.c_kickerSwitchPos5:
+                    kickerStrength = 5;
+                    break;
+                case Constants.c_kickerSwitchPos6:
+                    kickerStrength = 6;
+                    break;
+                case Constants.c_kickerSwitchPos7:
+                    kickerStrength = 7;
+                    break;
+                case Constants.c_kickerSwitchPos8:
+                    kickerStrength = 8;
+                    break;
             }
-            return kickerStrength;
+
+        } catch(DriverStationEnhancedIO.EnhancedIOException ex) {
+            ex.printStackTrace();
+        }
+        return kickerStrength;
 
     }
 
