e34c5596ddc1efaedcb3db4c47350e7d39f9ba52
andrew.elsen@yahoo.com <andrew.elsen@yahoo.com@ff96d080-0a19-11df-8158-c7cbc0dd6240>
2010-02-22 23:08:23 +0000

    Added lance raised/extended conditions Added printMessages() method Cleaned up some code

--- BreakawayRobot/com/team1987/breakaway/api/Constants.java
+++ BreakawayRobot/com/team1987/breakaway/api/Constants.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.team1987.breakaway.api;
 
 /**
@@ -12,26 +11,19 @@ package com.team1987.breakaway.api;
 public final class Constants {
     //this combined with the final makes the class uninstantiable.
 
-    private Constants(){
+    private Constants() {
     }
 
-    public static final int c_joystickRightPort = 2;
-    public static final int c_joystickLeftPort = 1;
-    
-    //Solenoids
-    public static final int c_LanceRaiseSolenoidInChannel = 4;
-    public static final int c_LanceRaiseSolenoidOutChannel = 3;
-    public static final int c_herderSolenoidsInChannel = 6;
-    public static final int c_herderSolenoidsOutChannel = 5;
-    public static final int c_kickerSolenoidInChannel = 1;
-    public static final int c_kickerSolenoidOutChannel = 2;
-
-    //Spikes
-    public static final int c_LanceExtenderSpikeChannel = 1;
-    public static final int c_combineSpikeChannel = 2;
-    public static final int c_compressorSpikeChannel = 3;
-
-
+    //Buttons
+    public static final int c_kickerRightButton = 1;
+    public static final int c_combineOffRightButton = 2;
+    public static final int c_combineReverseRightButton = 4;
+    public static final int c_combineForwardRightButton = 5;
+    public static final int c_LanceRaiseLeftButton = 6;
+    public static final int c_winchWindLeftButton = 8;
+    public static final int c_winchUnwindLeftButton = 9;
+    public static final int c_LanceRetractorLeftButton = 10;
+    public static final int c_LanceExtenderLeftButton = 11;
     //Digital Channels
     public static final int c_leftDriveEncoderDigitalChannel1 = 1;
     public static final int c_leftDriveEncoderDigitalChannel2 = 2;
@@ -40,41 +32,39 @@ public final class Constants {
     public static final int c_kickerEncoderDigitalChannel1 = 5;
     public static final int c_kickerEncoderDigitalChannel2 = 6;
     public static final int c_kickerWinderGearToothDigitalChannel = 7;
+    public static final int c_LanceExtendedDigitalChannel = 8;
     public static final int c_kickerSolenoidExtendedDigitalChannel = 9;
     public static final int c_kickerSolenoidReturnedDigitalChannel = 10;
     public static final int c_compressorPressureSwitchDigitalChannel = 11;
-    
     public static final int c_LanceRaisedDigitalChannel = 13;
     public static final int c_kickerWinderEmergencyStopDigitalChannel = 14;
-
-    
-    public static final int c_kickerEncoderMaxCounts = 0;
-
-    public static final double c_LanceExtendTime = 3;
-
-    //Motors
+    //Enhanced IO Channel
+    public static final int c_kickerLowTrajectoryIOChannel = 9;
+    //Joystick Ports
+    public static final int c_joystickRightPort = 2;
+    public static final int c_joystickLeftPort = 1;
+    //Kicker Winder
+    public static final double c_kickerStopWinding = 0;
+    public static final double c_kickerReturningSpeed = .15;
+    public static final double c_kickerWindingSpeed = .175;
+    //Motor PWM Channels
     public static final int c_leftDriveMotor1PWMChannel = 1;
     public static final int c_leftDriveMotor2PWMChannel = 2;
     public static final int c_rightDriveMotor1PWMChannel = 3;
     public static final int c_rightDriveMotor2PWMChannel = 4;
     public static final int c_kickerWinderPWMChannel = 5;
     public static final int c_winchPWMChannel = 6;
- 
-    //Kicker Winder
-    public static final double c_kickerStopWinding = 0;
-    public static final double c_kickerReturningSpeed = .2;
-    public static final double c_kickerWindingSpeed = .25;
-
-    //Buttons
-    public static final int c_kickerRightButton = 1;
-    public static final int c_winchStartLeftButton = 8;
-    public static final int c_LanceRaiseLeftButton = 6;
-    public static final int c_LanceExtenderLeftButton = 11;
-    public static final int c_LanceRetractorLeftButton = 10;
-    public static final int c_combineForwardRightButton = 5;
-    public static final int c_combineOffRightButton = 2;
-    public static final int c_combineReverseRightButton = 4;
-    
+    //Solenoid Channels
+    public static final int c_LanceRaiseSolenoidInChannel = 4;
+    public static final int c_LanceRaiseSolenoidOutChannel = 3;
+    public static final int c_herderSolenoidsInChannel = 6;
+    public static final int c_herderSolenoidsOutChannel = 5;
+    public static final int c_kickerSolenoidInChannel = 1;
+    public static final int c_kickerSolenoidOutChannel = 2;
+    //Spike Channels
+    public static final int c_LanceExtenderSpikeChannel = 1;
+    public static final int c_combineSpikeChannel = 2;
+    public static final int c_compressorSpikeChannel = 3;
     //Kicker 8pos Switch
     public static final int c_kickerSwitchBits = 255;
     public static final int c_kickerSwitchPos1 = 1;
@@ -85,9 +75,6 @@ public final class Constants {
     public static final int c_kickerSwitchPos6 = 32;
     public static final int c_kickerSwitchPos7 = 64;
     public static final int c_kickerSwitchPos8 = 128;
-    
-    public static final int c_kickerLowTrajectoryIOChannel = 9;
-
     //Kicker States
     public static final int c_kickerReady = 0;
     public static final int c_kickerKicking = 1;
@@ -95,8 +82,16 @@ public final class Constants {
     public static final int c_kickerReturning = 3;
     public static final int c_kickerLocked = 4;
     public static final int c_kickerWinding = 5;
-
+    //Kicker Print Messages
+    public static final String c_strKickerReady = "Kicker Ready";
+    public static final String c_strKickerKicking = "Kicker Kicking";
+    public static final String c_strKickerReleased = "Kicker Released";
+    public static final String c_strKickerReturning = "Returning";
+    public static final String c_strKickerLocked = "Kicker Locked";
+    public static final String c_strKickerWinding = "Kicker Winding";
+    //Random Constants (delays, etc)
     public static final int c_kickerDelay = 2;
-
+    public static final int c_kickerEncoderMaxCounts = 0;
+    public static final double c_LanceExtendTime = 3;
 }
 
