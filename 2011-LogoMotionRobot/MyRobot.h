3eba70ea4d57268306f532ecea36e024ddf29cab
nithlingx <nithlingx@ff96d080-0a19-11df-8158-c7cbc0dd6240>
2011-02-21 02:51:16 +0000

    decent state 8:51 Sun

--- 2011-LogoMotionRobot/MyRobot.h
+++ 2011-LogoMotionRobot/MyRobot.h
@@ -1,16 +1,16 @@
 #ifndef MYROBOT_H_
 #define MYROBOT_H_
 
-#define TRACKINGSPEED  .165;
-#define TRACKINGTURN .19;
+#define TRACKINGSPEED  -.2;
+#define TRACKINGTURN .2;
 #define ElevatorSpeed ;
 
-float Lvl1 = 180;
-float Lvl2 = 245;
-float Lvl3 = 190;
-float Lvl4 = 260;
-float Lvl5 = 535;
-float Lvl6 = 605;
+float Lvl1 = 180;		//lvl 1 outside
+float Lvl2 = 245;		//lvl 2 outside
+float Lvl3 = 190;		//lvl 1 middle
+float Lvl4 = 260;		//lvl 2 middle
+float Lvl5 = 535;		//lvl 3 outside
+float Lvl6 = 605;		//lvl 3 middle
 float Floor = 75;
 float EleMin = 70;
 float EleMax = 620;
@@ -20,12 +20,16 @@ float EleRange = (EleMax - EleMin);
 bool init = false;
 bool error = false; 
 bool EleAuto = true;
+bool ForkPass = false;
 
-int eleTol = 1;
+const int eleTol = 2;
 int EleCycle = 0;
-int NumCycle = 37;
+const int NumCycle = 50;
 int EMDir = 0;
-int EleCountStat = 1;
+const int EleErrorAdj = 50;
+const int LowEleDead = 60;
+const int HighEleDead = 700;
+bool EleCountStat = 1;
 
 class MyRobot;
 
